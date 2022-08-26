8e890071bfd14a7f9b3b7e60ef1ac2a0367550a7
shade <nkanaan1027@gmail.com>
2022-08-26 00:37:07 -0400

    Steam authentication

--- Core/Models/HydraAccessRequestBody.cs
+++ Core/Models/HydraAccessRequestBody.cs
@@ -1,6 +1,6 @@
 ﻿namespace HydraDotNet.Core.Models;
 
-public class HydraAccessRequestBody
+public class HydraEpicAccessRequestBody
 {
     public HydraAccessRequestAuth auth { get; set; } = new();
     public string[] options { get; set; } = new[]
@@ -17,6 +17,27 @@ public class HydraAccessRequestBody
     public class HydraAccessRequestAuth
     {
         public string? epic { get; set; }
+        public bool fail_on_missing { get; set; } = false;
+    }
+}
+
+public class HydraSteamAccessRequestBody
+{
+    public HydraAccessRequestAuth auth { get; set; } = new();
+    public string[] options { get; set; } = new[]
+    {
+        "configuration",
+        "achievements",
+        "account",
+        "profile",
+        "notifications",
+        "maintenance",
+        "wb_network"
+    };
+
+    public class HydraAccessRequestAuth
+    {
+        public string? steam { get; set; }
         public bool fail_on_missing { get; set; } = false;
     }
 }
